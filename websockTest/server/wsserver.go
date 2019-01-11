c0363488a203503da98f9b7b4cbfefaf6375e34b
songwei <songwei@matchvs.com>
2019-01-11 17:34:43 +0800

    websocket

--- websockTest/server/wsserver.go
+++ websockTest/server/wsserver.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -21,6 +22,14 @@ func NewWsServer() *WsServer {
 		ReadBufferSize:  4096,
 		WriteBufferSize: 1024,
 		CheckOrigin: func(r *http.Request) bool {
+			if r.Method != "GET" {
+				fmt.Println("method is not GET")
+				return false
+			}
+			if r.URL.Path != "/ws" {
+				fmt.Println("path error")
+				return false
+			}
 			return true
 		},
 	}
@@ -28,19 +37,82 @@ func NewWsServer() *WsServer {
 }
 
 func (self *WsServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method)
+
+	if r.URL.Path != "/ws" {
+		httpCode := http.StatusInternalServerError
+		reasePhrase := http.StatusText(httpCode)
+		fmt.Println("path error ", reasePhrase)
+		http.Error(w, reasePhrase, httpCode)
+		return
+	}
+
 	conn, err := self.upgrade.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println("websocket error:", err)
 		return
 	}
+	fmt.Println("client connect :", conn.RemoteAddr())
+	go self.connHandle(conn)
+
+}
+
+func (self *WsServer) connHandle(conn *websocket.Conn) {
+	defer func() {
+		conn.Close()
+	}()
+	stopCh := make(chan int)
+	go self.send(conn, stopCh)
 	for {
-		mty, msg, err := conn.ReadMessage()
+		conn.SetReadDeadline(time.Now().Add(time.Millisecond * time.Duration(5000)))
+		_, msg, err := conn.ReadMessage()
 		if err != nil {
+			close(stopCh)
+			// 判断是不是超时
+			if netErr, ok := err.(net.Error); ok {
+				if netErr.Timeout() {
+					fmt.Printf("ReadMessage timeout remote: %v\n", conn.RemoteAddr())
+					return
+				}
+			}
+			// 其他错误，如果是 1001 和 1000 就不打印日志
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseNormalClosure) {
+				fmt.Printf("ReadMessage other remote:%v error: %v \n", conn.RemoteAddr(), err)
+			}
 			return
 		}
 		fmt.Println("收到消息：", string(msg))
-		conn.WriteMessage(mty, []byte("hello websocket test"))
+	}
+}
+
+//测试一次性发送 10万条数据给 client, 如果不使用 time.Sleep browser 过了超时时间会断开
+func (self *WsServer) send10(conn *websocket.Conn) {
+	for i := 0; i < 100000; i++ {
+		data := fmt.Sprintf("hello websocket test from server %v", time.Now().UnixNano())
+		err := conn.WriteMessage(1, []byte(data))
+		if err != nil {
+			fmt.Println("send msg faild ", err)
+			return
+		}
+		// time.Sleep(time.Millisecond * 1)
+	}
+}
+
+func (self *WsServer) send(conn *websocket.Conn, stopCh chan int) {
+	self.send10(conn)
+	for {
+		select {
+		case <-stopCh:
+			fmt.Println("connect closed")
+			return
+		case <-time.After(time.Second * 1):
+			data := fmt.Sprintf("hello websocket test from server %v", time.Now().UnixNano())
+			err := conn.WriteMessage(1, []byte(data))
+			fmt.Println("sending....")
+			if err != nil {
+				fmt.Println("send msg faild ", err)
+				return
+			}
+		}
 	}
 }
 
