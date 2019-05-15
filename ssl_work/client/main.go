06b55baadf6df77c24c97908d6e4d420f8a44ec6
ville <songwei@mintechai.com>
2019-05-15 14:48:31 +0800

    ssl

--- ssl_work/client/main.go
+++ ssl_work/client/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
+func addTrust(pool*x509.CertPool, path string) {
+	aCrt, err := ioutil.ReadFile(path)
+	if err!= nil {
+		fmt.Println("ReadFile err:",err)
+		return
+	}
+	pool.AppendCertsFromPEM(aCrt)
+}
 
-func main(){
+
+
+func UnCheckServerSSL()  {
 	tr := &http.Transport{
 		TLSClientConfig:    &tls.Config{InsecureSkipVerify: true},
 	}
@@ -20,4 +31,39 @@ func main(){
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	fmt.Println(string(body))
+}
+
+func TwoWaySSlCheck(){
+	pool := x509.NewCertPool()
+
+	addTrust(pool,"../sslfile/server.crt")
+
+	cliCrt, err := tls.LoadX509KeyPair("../sslfile/ville_pem.crt", "../sslfile/ville.key")
+
+
+	if err != nil {
+		fmt.Println("Loadx509keypair err:", err)
+		return
+	}
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			RootCAs:      pool,
+			Certificates: []tls.Certificate{cliCrt},
+		},
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Get("https://localhost:8080")
+	if err != nil {
+		fmt.Println("Get error:", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+}
+
+func main(){
+	TwoWaySSlCheck()
+	//UnCheckServerSSL()
 }
--- ssl_work/server/main.go
+++ ssl_work/server/main.go
@@ -1,15 +1,27 @@
 
 package main
+
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
-"net/http"
+	"net/http"
 )
 
 type httpsHandler struct {
+}
+
+func addTrust(pool*x509.CertPool, path string) {
+	aCrt, err := ioutil.ReadFile(path)
+	if err!= nil {
+		fmt.Println("ReadFile err:",err)
+		return
+	}
+	pool.AppendCertsFromPEM(aCrt)
+
 }
 
 func (*httpsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -18,14 +30,11 @@ func (*httpsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	pool := x509.NewCertPool()
-	caCertPath := "../sslfile/root.crt"
+	// 这里是加载客户端提供的证书，最好是加载客户端提供的根证书
+	addTrust(pool,"../sslfile/ca.crt")
+	addTrust(pool,"../sslfile/selfsigned.crt")
+	addTrust(pool,"../sslfile/ville_pem.crt")
 
-	caCrt, err := ioutil.ReadFile(caCertPath)
-	if err != nil {
-		log.Fatal("ReadFile err:", err)
-		return
-	}
-	pool.AppendCertsFromPEM(caCrt)
 
 	s := &http.Server{
 		Addr:    ":8080",
@@ -36,7 +45,7 @@ func main() {
 		},
 	}
 
-	if err = s.ListenAndServeTLS("../sslfile/server.crt", "../sslfile/server.key"); err != nil {
+	if err := s.ListenAndServeTLS("../sslfile/server.crt", "../sslfile/server.key"); err != nil {
 		log.Fatal("ListenAndServeTLS err:", err)
 	}
 }
